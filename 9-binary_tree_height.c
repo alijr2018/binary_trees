16a2e1b3e6bb4f73bc0bfbe7837481c4c4d0e0ed
alijr2018 <alirami120@hotmail.fr>
2023-08-31 13:54:20 +0100

    done

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,20 +8,20 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l = 0;
-	size_t r = 0;
+	size_t i, j;
 
+	i = 0;
+	j = i;
 	if (tree == NULL)
-	{
 		return (0);
-	}
 	else
 	{
 		if (tree)
 		{
-			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+			i = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			j = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
-		return ((l > r) ? l : r);
+		return ((i > j) ? i : j);
 	}
 }
+
