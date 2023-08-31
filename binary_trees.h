dc290ef3c1eca4dbf2ad8a9f8e76cb5328c756df
alijr2018 <alirami120@hotmail.fr>
2023-08-31 11:25:25 +0100

    done

--- binary_trees.h
+++ binary_trees.h
@@ -20,6 +20,13 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+/**Binary Search Tree**/
+typedef struct binary_tree_s bst_t;
+/**AVL Tree**/
+typedef struct binary_tree_s avl_t;
+/**Max Binary Heap**/
+typedef struct binary_tree_s heap_t;
+
 /**Tasks**/
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -38,5 +45,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 #endif /* _BINARY_TREES_H_ */
