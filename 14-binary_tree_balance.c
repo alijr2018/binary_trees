54f78d3bf95c152c049b7c5edb47afdf0690ff08
alijr2018 <alirami120@hotmail.fr>
2023-08-31 14:28:51 +0100

    done

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -18,8 +18,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 	else
 	{
-		i = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		j = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		i = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		j = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((i > j) ? i : j);
 	}
 }
