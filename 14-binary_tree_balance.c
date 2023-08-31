57bffc53ed5505931a21409071f62c50258d6b02
alijr2018 <alirami120@hotmail.fr>
2023-08-31 14:14:41 +0100

    done

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,30 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - Function that measures the height of a binary tree
+ * @tree: tree to go through
+ * Return: the height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t i, j;
+
+	i = 0;
+	j = i;
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		i = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		j = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((i > j) ? i : j);
+	}
+}
+
+/**
  * binary_tree_balance - Measures the balance factor of a binary tree.
  * @tree: Pointer to the root node of the tree to measure the balance factor.
  * Return: The balance factor of the tree, or 0 if tree is NULL.
