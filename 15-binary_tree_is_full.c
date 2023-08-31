349bb50d1d96c59f31302df9da373fbfc8929b4e
alijr2018 <alirami120@hotmail.fr>
2023-08-31 14:40:46 +0100

    done

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -20,6 +20,19 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		{
 			left = binary_tree_is_full(tree->left);
 			right = binary_tree_is_full(tree->right);
+			if (left == 0 || right == 0)
+			{
+				return (0);
 			}
+			return (1);
+		}
+		else if (!tree->left && !tree->right)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
 	}
 }
